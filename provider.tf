535b31e0123409f30294d20e9711e61977e5393c
akhilannn <56840040+akhilannn@users.noreply.github.com>
2023-06-13 20:24:37 -0400

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  version         = "3.0.0"
+  client_id       = "658043bc-298e-4146-8c90-248e8ae864c4"
+  client_secret   = "v.l8Q~VLEcErbnMg7snynZrcA-.ArH_btF_yGdd1"
+  tenant_id       = "b49893ce-21b4-4108-9ab9-33bc41884b09"
+  subscription_id = "b49893ce-21b4-4108-9ab9-33bc41884b09"
+}
---
+++ testpipline.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "example" {
+  name     = "vm-rg"
+  location = "uk south"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vm-network"
+  address_space       = ["12.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["12.0.2.0/24"]
+}
+resource "azurerm_public_ip" "example" {
+  name                = "vmpIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
