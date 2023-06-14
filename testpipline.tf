4b8841e6040cef1e3b6f102db372d134e6e46e61
akhilannn <56840040+akhilannn@users.noreply.github.com>
2023-06-13 21:51:38 -0400

    Update testpipline.tf

--- testpipline.tf
+++ testpipline.tf
@@ -21,3 +21,4 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
+}
