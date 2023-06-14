8e14544dfcdc679820bc229a1b15196c35fff510
akhilannn <56840040+akhilannn@users.noreply.github.com>
2023-06-13 22:24:34 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features {}
   version         = "3.0.0"
-  client_id       = "658043bc-298e-4146-8c90-248e8ae864c4"
-  client_secret   = "v.l8Q~VLEcErbnMg7snynZrcA-.ArH_btF_yGdd1"
+  client_id       = "8c85bb27-6073-4730-8453-233ed220660f"
+  client_secret   = "WGn8Q~_JvgvkMnYk8UqLPR8yoD.6YmNO9Om3BdaS"
   tenant_id       = "b49893ce-21b4-4108-9ab9-33bc41884b09"
-  subscription_id = "b49893ce-21b4-4108-9ab9-33bc41884b09"
+  subscription_id = "933884d4-cfa7-4565-8f4c-71dff46c3635"
 }
 terraform {
   backend "azurerm" {
